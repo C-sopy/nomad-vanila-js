4675ba4993c4b1ece1ea0c24f75c33dfc4bfb723
cheyeon.shin <cheyeon.shin@lgepartner.com>
2024-09-03 17:26:49 +0900

    todolist 만드는중...⌨🖱

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,11 @@
 const toDoForm = document.getElementById("todos-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todos-list");
-const toDos = [];
+let toDos = [];
 const TODOS = "todos";
 
 function saveTodos() {
-  localStorage.setItem(TODOS, JSON.stringify(toDos));
+  localStorage.setItem(TODOS, JSON.stringify(toDos)); // JSON.stringify : string으로 만들기 & JSON.parse : 배열로 만들기
 }
 
 function deleteTodo(event) {
@@ -16,8 +16,9 @@ function deleteTodo(event) {
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const btn = document.createElement("button");
   btn.innerText = "❌";
   btn.addEventListener("click", deleteTodo);
@@ -30,11 +31,21 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintTodo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintTodo(newTodoObj);
   saveTodos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
 
 const savedTodos = localStorage.getItem(TODOS);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  toDos = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
