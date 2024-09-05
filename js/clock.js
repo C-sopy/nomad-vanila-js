a71a9153ac0b17c5b5ddd54f8c31b2b2ed6e391c
cheyeon.shin <cheyeon.shin@lgepartner.com>
2024-09-05 16:55:54 +0900

    css 손보는중...

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector("h2#clock");
+const date = document.querySelector("h2#date");
 
 function getClock() {
   const date = new Date();
@@ -9,6 +10,14 @@ function getClock() {
 
   clock.innerText = `${hour}:${min}:${sec}`;
 }
+
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDay();
+
+date.innerText = `${year}년 ${month}월 ${day}일`;
 
 getClock();
 setInterval(getClock, 1000);
